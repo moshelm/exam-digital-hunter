1bf42f61df1377486c308c68e318159cdcf48563
Moshe <moshe@example.com>
2026-03-16 10:34:52 +0200

    feat: adding basic for producer in kafka

--- shared/kafka/producer.py
+++ shared/kafka/producer.py
@@ -1,6 +1,6 @@
-from confluent_kafka import Producer
+from confluent_kafka import Producer, Message
 from logging import Logger 
-
+from utils.serializer import serialize_json
 
 class KafkaProducer():
     def __init__(self,kafka_config:dict, topic_name:str, logger:Logger):
@@ -12,5 +12,24 @@ class KafkaProducer():
             self.logger.critical('failed connect to kafka')
             raise
         self.topic = topic_name
+
+    def run(self, data):
+        try:
+            value = serialize_json(data,self.logger)
+            self.producer.produce(self.topic,value, callback=self.delivery)
+            self.logger.info('new event send in success')
+        except Exception:
+            self.logger.error("failed to sent new event",exc_info=True)
+    def delivery(self, err: Message, msg:Message):
+        if err:
+            self.logger.error(f'failed to sent new event.{err}')
+        else:
+            self.logger.info('success sending new event')
+    def flush(self):
+        try:
+            self.producer.flush()
+            self.logger.info('flushing all events')
+        except Exception:
+            self.logger.error('failed to flush')
 
 
