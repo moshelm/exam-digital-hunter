622b4435d6032e8cec85fbeaa637b3e0cd6e1fec
Moshe <moshe@example.com>
2026-03-16 15:50:14 +0200

    feat: create config file

---
+++ extract-data-server/app/server_config.py
@@ -0,0 +1,22 @@
+import os 
+
+
+
+class Configuration():
+    def __init__(self):
+        mongodb_host = os.getenv("MONGODB_HOST",'localhost') 
+        mongodb_port = os.getenv("MONGODB_PORT",'27017') 
+        self.mongodb_database = os.getenv("MONGODB_DATABASE",'digital_hunter') 
+        self.mongodb_collection_base = os.getenv("MONGODB_COLLECTION",'targets bank') 
+        
+        kafka_host = os.getenv("KAFKA_HOST",'localhost') 
+        kafka_port = os.getenv("KAFKA_PORT",'27017') 
+        kafka_group_id = os.getenv('KAFKA_GROUP_ID','extract')
+        
+        self.kafka_producer_topic = os.getenv('KAFKA_PRODUCER_TOPIC','')
+        self.kafka_consumer_topics = os.getenv('KAFKA_CONSUMER_TOPICS','').split(',')
+
+        self.kafka_bootstrap = {'bootstrap.servers':f'{kafka_host}:{kafka_port}'}
+        self.kafka_group = {"group.id": kafka_group_id}
+
+        self.mongodb_config = f'mongodb://{mongodb_host}:{mongodb_port}'
