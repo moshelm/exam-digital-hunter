f4727cbc0a2dfb197eadfab5d79dda107cd42754
Moshe <moshe@example.com>
2026-03-16 16:05:23 +0200

    feat:add main

---
+++ extract-data-server/app/main.py
@@ -0,0 +1,26 @@
+from shared.logger import log_event
+from shared.kafka.consumer import KafkaConsumer
+from shared.kafka.producer import KafkaProducer
+from shared.mongodb_manager import MongodbManager
+from server_config import Configuration
+from orchestrator import Orchestrator
+from validate_manager import Validator
+
+config = Configuration()
+
+def main():
+    try:
+        mongodb = MongodbManager(config.mongodb_config,config.mongodb_database)
+        producer = KafkaProducer(config.kafka_bootstrap,config.kafka_producer_topic)
+        consumer = KafkaConsumer(config.kafka_bootstrap, config.kafka_group, config.kafka_consumer_topics)
+        validator = Validator(config.kafka_consumer_topics)
+
+        manager = Orchestrator(mongodb, producer, consumer, config.mongodb_collection_base, validator)
+
+        manager.run()
+    except Exception:
+        log_event()
+if __name__=='__main__':
+    main()
+
+
